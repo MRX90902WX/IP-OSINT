e1c8c1ea7f5a64282bcfaa172b3466236bd64f30
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-02-12 19:42:56 -0500

    Update osint.sh

--- osint.sh
+++ osint.sh
@@ -18,9 +18,10 @@ echo -e "\e[1;37m[\e[0m\e[1;31m4\e[0m\e[1;37m]. Hacer osint a una IP Publica"
 echo -e "\e[1;37m[\e[0m\e[1;31m5\e[0m\e[1;37m]. Ver info de un numero de telefono"
 echo -e "\e[1;37m[\e[0m\e[1;31m6\e[0m\e[1;37m]. Comprobar si el puerto 22 está abierto"
 echo -e "\e[1;37m[\e[0m\e[1;31m7\e[0m\e[1;37m]. Mostrar la versión remota del servidor SSH"
-echo -e "\e[1;37m[\e[0m\e[1;31m8\e[0m\e[1;37m]. Escanear puertos vulnerables de una IP"
-echo -e "\e[1;37m[\e[0m\e[1;31m9\e[0m\e[1;37m]. Phishing wifi"
-echo -e "\e[1;37m[\e[0m\e[1;31m00\e[0m\e[1;37m]. Salir\e[0m"
+echo -e "\e[1;37m[\e[0m\e[1;31m8\e[0m\e[1;37m]. Saber la IP de una pagina\e[0m"
+echo -e "\e[1;37m[\e[0m\e[1;31m9\e[0m\e[1;37m]. Escanear puertos vulnerables de una IP"
+echo -e "\e[1;37m[\e[0m\e[1;31m10\e[0m\e[1;37m]. Phishing wifi"
+echo -e "\e[1;37m[\e[0m\e[1;31m99\e[0m\e[1;37m]. Salir\e[0m"
 echo -e -n "\e[1;37m[+]\e[0m\e[1;31mOpcion >>\e[0m "
 read opcion
 case $opcion in
@@ -130,18 +131,25 @@ exit
 8)
 #! /bin/bash
 echo ""
+cd SABER-IP-DE-UNA-PAGINA
+python ip.py
+exit
+;;
+9)
+#! /bin/bash
+echo ""
 cd Escanear-port
 python port1.py
 exit
 ;;
-9)
+10)
 echo ""
 #! /bin/bash
 cd PhishW
 bash wifi.sh
 exit
 ;;
-00)
+99)
 #! /bin/bash
 setterm -foreground yellow
 echo "[~] Un gusto que uses mi herramienta , bye."
