c0864103a8c6ac76e411015e988827a5b012d969
MRX90902WX <118016739+MRX90902WX@users.noreply.github.com>
2023-02-13 17:36:50 -0500

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 while :
 do
-echo -e "\e[1;37m*****************************************\e[0m"
-echo -n -e "\e[1;32mDesea regresar al menu\e[0m \e[1;37m[\e[0m\e[1;31msi\e[0m\e[1;37m]\e[0m \e[1;32msalir\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m>\e[0m \e[1;37m\e[0m"
+echo -e "\e[1;37m-------------------------------------------\e[0m" 
+echo -n -e "\e[1;32mDesea regresar al menu\e[0m \e[1;37m[\e[0m\e[1;31msi\e[0m\e[1;37m]\e[0m \e[1;32msalir\e[0m \e[1;37m[\e[0m\e[1;31mno\e[1;37m]\e[0m \e[1;32m》\e[0m \e[1;37m\e[0m"
 read opcion
 case $opcion in
 si)
